765d4c7d18a57dd85ff32cc7166fc2acaa971083
linj4653 <linj4653@HRH-IC0027333.wrdsb.ca>
2017-03-03 10:09:21 -0500

    Assignment 3 final draft

--- src/A3Q6.java
+++ src/A3Q6.java
@@ -125,13 +125,9 @@ public class A3Q6 {
                 }
             }
             //stopping at (4, 1)
-        if (timmy.getStreet() == 4 && timmy.getAvenue() == 1) {
-            break;
-        }
+            if (timmy.getStreet() == 4 && timmy.getAvenue() == 1) {
+                break;
+            }
         }
     }
 }
-        
-        
-
-    
